e523bf3d20d848a23c60c4e076189c968ccd3ea5
boonjun123654 <boonjunlee@gmail.com>
2025-05-13 21:27:31 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -62,11 +62,11 @@ async def handle_mode_select(update: Update, context: ContextTypes.DEFAULT_TYPE)
     mode = query.data.split(":")[1]
     chat_id = query.message.chat.id
     group_mode[chat_id] = mode
-    if mode == "wenchi":
-        bad_food = random.randint(1, 10)
-        group_data[chat_id] = {"bad": bad_food, "selected": set()}
-        await context.bot.send_photo(chat_id=chat_id, photo=START_IMAGE_WenChi, caption="😋 WenChi 今天吃什么？请选择：",
-        reply_markup=get_food_keyboard())
+    elif mode == "wenchi":
+        bad = random.randint(1, 10)
+        group_data[chat_id] = {"bad": bad, "selected": set()}
+        await context.bot.send_photo(chat_id=chat_id, photo=START_IMAGE_WenChi, caption="😋 WenChi 今天吃什么？游戏开始！")
+        await context.bot.send_message(chat_id=chat_id, text="😋 WenChi 今天吃什么？请选择：", reply_markup=get_food_keyboard())
 
     elif mode == "sweeper":
         group_data[chat_id] = {"min": 1, "max": 100, "bomb": random.randint(1, 100)}
@@ -116,6 +116,7 @@ async def handle_restart(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=chat_id, text="😋 WenChi 今天吃什么？请选择：", reply_markup=get_food_keyboard())
 
 async def handle_main_menu(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    query = update.callback_query
     await query.message.edit_reply_markup(reply_markup=None)
     await send_main_menu(update.effective_chat.id, context)
 
